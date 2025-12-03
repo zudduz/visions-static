b91447284f8d840d96450696b8961ebe797cf77d
zudduz <zudduz@gmail.com>
2025-12-03 07:15:47 +0000

    Improve deploy file

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+if ! firebase projects:list > /dev/null 2>&1; then
+  echo "You are not logged into Firebase. Please run 'firebase login' and try again."
+  exit 1
+fi
 firebase deploy --public static --only hosting
